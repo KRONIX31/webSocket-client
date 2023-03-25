8ab476fa3bb0645fd65042bbca14d0ae17b57d47
KR <78811949+KRONIX31@users.noreply.github.com>
2023-03-25 18:58:04 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const submitButton = document.querySelector('.submit_wrapper button')
 const submitInput = document.querySelector('.submit_wrapper input')
 const formWrapper = document.querySelector('.form_wrapper')
 const myNicknameInput = document.querySelector('.my_nickname_wrapper input')
-myNicknameInput.value = 'LEXA'
+myNicknameInput.value = JSON.parse(localStorage.WebSocket).nickname || ''
 const chat = document.querySelector('.chat')
 
+
+myNicknameInput.addEventListener('change',(e)=>{
+    localStorage.setItem('WebSocket',  JSON.stringify({'nickname': myNicknameInput.value}))
+})
 function send(){
     const name = myNicknameInput.value
     const message = submitInput.value
